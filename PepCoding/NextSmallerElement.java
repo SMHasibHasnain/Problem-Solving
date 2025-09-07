f14a7088fe64532ff7905f079b699361312acc68
Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2025-09-08 00:19:33 +0600

    Solved the incorrect organization of results on NextSmallerElement problem my storing outputs on an another array

--- PepCoding/NextSmallerElement.java
+++ PepCoding/NextSmallerElement.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class NextSmallerElement {
     public static void main(String[] args) {
-        int[] arr = [4, 5, 3, 2, 6, 1, 7, 2, 3, 2];
+        int[] arr = {4, 5, 3, 2, 6, 1, 7, 2, 3, 2};
+        int[] arr2 = new int[arr.length];
+
         Stack<Integer> stack = new Stack<>();
 
         stack.push(0);
         for(int i=1; i<arr.length; i++) {
             if(arr[i]<arr[stack.peek()]) {
                 while(!stack.isEmpty() && arr[i]<arr[stack.peek()]) {
-                    stack.pop();
-                    System.out.println(arr[i]);
+                    arr2[stack.pop()] = arr[i];
                 }
                 stack.push(i);
             } else {
@@ -18,8 +19,8 @@ public class NextSmallerElement {
             }
         }
         while(!stack.isEmpty()) {
-            stack.pop();
-            System.out.println("-1");
+            arr2[stack.pop()] = -1;
         }
+        System.out.println(Arrays.toString(arr2));
     }
 }
