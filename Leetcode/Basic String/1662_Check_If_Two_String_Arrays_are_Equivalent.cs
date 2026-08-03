387f763333ab04831d6ebc8e685d1871ff15e9d8
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-08-04 00:02:06 +0600

    Add header comment for Check If Two String Arrays are Equivalent solution

--- Leetcode/Basic String/1662_Check_If_Two_String_Arrays_are_Equivalent.cs
+++ Leetcode/Basic String/1662_Check_If_Two_String_Arrays_are_Equivalent.cs
@@ -1,3 +1,11 @@
+/*
+    * 1662. Check If Two String Arrays are Equivalent
+    * https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/
+    * Easy
+    * Runtime: 0ms Beats 100.00%
+    * TC: O(n)
+*/
+
 public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
         string one = "";
